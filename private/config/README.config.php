90482945eb08bdde3474b196d4d534580eab2f90
sergio <sergio.atwork69@gmail.com>
2020-01-25 21:43:40 +0100

    cv.sergio-webdev.com 1.0.1

--- private/config/README.config.php
+++ private/config/README.config.php
@@ -1,13 +1,10 @@
 <?php
-    $db_name = '';
-    $db_id = '';
-    $db_pass = '';
-
-    $smtp_host = '';
-    $smtp_id   = '';
-    $smtp_pw   = '';
-    $smtp_port = 587;
-    $smtp_from = '';
-    $smtp_name = '';
-    $smtp_to   = '';
-    $smtp_subject = '';
+// Settings for use with PHPMailer
+    $smtp_host = '';    // SMTP server name
+    $smtp_id   = '';    // SMTP server username
+    $smtp_pw   = '';    // SMTP server password
+    $smtp_port = 587;   // SMTP server TCP port
+    $smtp_from = '';    // From email address
+    $smtp_name = '';    // From email name
+    $smtp_to   = '';    // Email address where you want to receive the message posted on the site
+    $smtp_subject = ''; // Email subject
