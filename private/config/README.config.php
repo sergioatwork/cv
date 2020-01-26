7490bebf0022f1e57955bfa8b2fc0e36159a8be2
sergio <sergio.atwork69@gmail.com>
2020-01-26 13:12:34 +0100

    Deplacement de l'initialisation des erreurs dans config.php

--- private/config/README.config.php
+++ private/config/README.config.php
@@ -1,10 +1,17 @@
 <?php
-// Settings for use with PHPMailer
-    $smtp_host = '';    // SMTP server name
-    $smtp_id   = '';    // SMTP server username
-    $smtp_pw   = '';    // SMTP server password
-    $smtp_port = 587;   // SMTP server TCP port
-    $smtp_from = '';    // From email address
-    $smtp_name = '';    // From email name
-    $smtp_to   = '';    // Email address where you want to receive the message posted on the site
-    $smtp_subject = ''; // Email subject
+
+//// Initialisation des erraurs
+error_reporting(E_ALL);
+ini_set("display_errors", TRUE);
+ini_set('log_errors', TRUE);
+ini_set('error_log', '../private/errors.log');
+
+//// Initialisation des paramètres SMTP pour l'utilisation avec PHPMailer
+$smtp_host = '';    // SMTP server name
+$smtp_id   = '';    // SMTP server username
+$smtp_pw   = '';    // SMTP server password
+$smtp_port = 587;   // SMTP server TCP port
+$smtp_from = '';    // From email address
+$smtp_name = '';    // From email name
+$smtp_to   = '';    // Email address where you want to receive the message posted on the site
+$smtp_subject = ''; // Email subject
